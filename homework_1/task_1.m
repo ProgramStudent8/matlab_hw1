9e9d2e405dcff2c5058b6d1734a23b3140a8ab9e
ProgramStudent8 <echick06@gmail.com>
2024-09-27 15:48:13 +0000

    solved the first one

---
+++ homework_1/task_1.m
@@ -0,0 +1,15 @@
+% Author Name: Eric Ciccarelli 
+% Email: ciccar47@rowan.edu
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Homework 1
+
+% 1. Variable Operations
+a) Create a variable `radius` and assign it a value of 5.
+%creating variables
+r = 5 %raduis of circle
+%calculation of area
+%caclulating circumfrence of the circle
+
+b) Calculate the area of a circle with this radius and store it in a variable called `area`.
+c) Calculate the circumference of the circle and store it in a variable called `circumference`.
+d) Display the results with appropriate labels.
